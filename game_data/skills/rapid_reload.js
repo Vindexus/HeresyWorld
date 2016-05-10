d7aa55fb9c50f23ff4ed9713e2b8fcd5a7abdc4c
unknown <vindexus@gmail.com>
2016-05-09 23:52:32 -0700

    Added rapid reload Game Data file.

--- game_data/skills/rapid_reload.js
+++ game_data/skills/rapid_reload.js
@@ -1,3 +1,3 @@
 module.exports = {
-  description: 'When you <strong>attempt to reload quickly roll+DEX</strong>. On a 10+ you reload your weapon instantly - reset its uses and reduce your ammo by 1. On a 7-9 the same in addition to what that the GM says. On a 6- you fumble your reload.'
+  description: 'When you <strong>attempt to reload quickly roll+DEX</strong>. On a 10+ you reload your weapon instantly - reset its uses and reduce your ammo by one. On a 7-9 the same in addition to what that the GM says. On a 6- you fumble your reload.'
 };
