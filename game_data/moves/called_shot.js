6af7945d36622d6ac0ea590683e2385b3d30b12c
unknown <vindexus@gmail.com>
2016-05-14 18:08:20 -0700

    Added three moves.

---
+++ game_data/moves/called_shot.js
@@ -0,0 +1,21 @@
+module.exports = {
+	description: '<p aid:pstyle="NoIndent">When you <strong>attack a defenseless or surprised enemy at range</strong>, you can choose to deal your damage or name your target and roll+DEX.</p>' + 
+		'<ul aid:pstyle="MoveOptions">' + 
+		'<li><strong>Head</strong>' + 
+			'<ul aid:pstyle="MoveSubOptions"><li>10+: As 7-9, plus your damage</li>' +
+			'<li>7-9: They do nothing but stand and drool for a few moments.</li></ul></li>' + 
+		'<li><strong>Arms</strong>' +
+			'<ul aid:pstyle="MoveSubOptions"><li>10+: As 7-9, plus your damage</li>' + 
+			'<li>7-9: They drop anything they\'re holding</li></ul></li>' +
+		'<li><strong>Legs</strong>' + 
+			'<ul aid:pstyle="MoveSubOptions"><li>10+: As 7-9, plus your damage</li>' + 
+			'<li><strong>Head</strong>' +
+				'<ul><li>10+: As 7-9, plus your damage</li>' +
+				'<li>7-9: They do nothing but stand and drool for a few moments.</li></ul></li>'+
+			'<li><strong>Arms</strong>' +
+				'<ul><li>10+: As 7-9, plus your damage</li>' +
+				'<li>7-9: They drop anything they\'re holding</li></ul></li>'+
+			'<li><strong>Legs</strong>' +
+				'<ul><li>10+: As 7-9, plus your damage</li>' +
+				'<li>7-9: They\'re hobbled and slow moving.</li></ul></li></ul>'
+}
