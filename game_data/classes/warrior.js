6fbbc1a62f253ef7ea36579fc8a4d40aa9e7226a
unknown <vindexus@gmail.com>
2016-05-17 18:12:54 -0700

    Created warrior.js file.

---
+++ game_data/classes/warrior.js
@@ -0,0 +1,19 @@
+module.exports = {
+  starting_hp: '10+CON',
+  damage: 'd8',
+  load: '9+STR',
+  looks: ['Blank eyes, fiery eyes, kind eyes', 'Military cut, messy hair, headband, helmet', 'Worn uniform, utilitarian clothes, rags', 'Lean body, hulking body, lithe body'],
+  starting_moves: ['grab_grenade', 'explosives_expert', 'first_hand_experience'],
+  alignments: {
+    good: {
+      name: 'Good',
+      description: 'Show mercy to an enemy.'
+    },
+    anarchic: {
+      name: 'Anarchic',
+      description: 'Rush into danger despite the plan.'
+    }
+  },
+  bonds: ['<blank></blank> would make a good soldier. I should show them I respect them.', '<blank></blank> has trouble keeping focused in tense situations. Perhaps I can help them with that.', '<blank></blank> has saved me from myself in combat, I owe them.'],
+  starting_gear: '<p>You get nothing.</p>'
+};
