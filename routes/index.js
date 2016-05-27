97eeb5a90f6cae3da43c6e412b1f0c59179fe8d2
unknown <vindexus@gmail.com>
2016-05-26 23:02:18 -0700

    Moved files around so website was in root directory.

--- routes/index.js
+++ routes/index.js
@@ -11,8 +11,13 @@ function renderParsedPage(pageFile, res, params) {
 
 /* GET home page. */
 router.get('/:page', function(req, res, next) {
-  var pageFile = path.join(__dirname + '/../../parsed/web/' + req.params.page + '.html.html');
+  var pageFile = path.join(__dirname + '/../parsed/web/' + req.params.page + '.html.html');
   renderParsedPage(pageFile, res, {title: req.params.page});
+});
+
+router.get('/', , function(req, res, next) {
+  var pageFile = path.join(__dirname + '/../parsed/web/introduction.html.html');
+  renderParsedPage(pageFile, res, {title: 'Introduction'});
 });
 
 module.exports = router;
