f59629a37ecc8ab6e98121232edf40508f38ccf8
unknown <vindexus@gmail.com>
2016-06-01 19:08:47 -0700

    Special Moves pages started.

---
+++ game_data/moves/level_up.js
@@ -0,0 +1,2 @@
+module.exports = '<p class="no-indent">When you <strong>have downtime (hours or days) and XP equal to (or greater than) your current level + 7</strong>, subtract your current level +7 from your XP, increase your level by 1, and choose a new advanced move. Choose from your class or the general advanced moves.</p>\n' + 
+'<p>Choose one of your stats and increase it by 1 (this may change your modifier). Upgrading your Constitution increases your maximum wounds by 1. Ability scores can\'t go higher than 18.</p>'
