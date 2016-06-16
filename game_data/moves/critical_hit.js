8add217310e631bc3c6d27e42de189fe83e839b7
Colin Kierans <vindexus@gmail.com>
2016-06-16 11:48:08 -0700

    Added critical hit move.

---
+++ game_data/moves/critical_hit.js
@@ -0,0 +1,13 @@
+module.exports = {
+  description: '<p class="no-indent">When you <strong>gain a critical wound</strong>, roll+CON and subtract the number of critical wounds you have. On a 10+ choose one from the following list. On a 7-9 the GM will choose one.</p>' + 
+  '\n<ul><li>You lose your footing</li>' +
+  '\n<li>You are blinded</li>' +
+  '\n<li>You don’t notice something important</li>' +
+  '\n<li>You drop something</li>' +
+  '\n<li>You take an additional wound</li></ul>' + 
+  '\n<p class="no-indent">On a 6- choose one from the list below. Describe your new scar and write it on your sheet.</p>' +
+  '\n<ul><li>You are knocked unconscious</li>' +
+  '\n<li>A limb is broken or maimed</li>' +
+  '\n<li>You gain a debility</li>' +
+  '\n<li>You won’t heal from resting, you need medical aid</li></ul>'
+}
