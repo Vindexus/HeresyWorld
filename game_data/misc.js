42724e7cf46bca94567793cc1a540c2bb76aaef2
unknown <vindexus@gmail.com>
2016-06-16 19:50:31 -0700

    Critical hit to list

--- game_data/misc.js
+++ game_data/misc.js
@@ -1,6 +1,6 @@
 module.exports = {
   advanced_moves: 'rapid_reload,hatred,parry,mighty_shot,crushing_blow,dual_wield,gunslinger,medicae_training,imperial_infantrymans_uplifting_primer,autosanguine,advanced_tech_use,shield_weapon,shield_wall,sacrifishield'.split(','),
-  special_moves: 'last_breath,encumbrance,make_camp,take_watch,undertake_a_perilous_journey,level_up,supply,end_of_session,recover,bolster,this_i_know'.split(','),
+  special_moves: 'last_breath,encumbrance,make_camp,take_watch,undertake_a_perilous_journey,level_up,supply,end_of_session,recover,bolster,this_i_know,critical_hit'.split(',').sort(),
   basic_moves: ['hack_and_slash', 'aim_and_fire', 'suppressing_fire', 'reload', 'throw_grenade', 'defy_danger', 'defend', 'spout_lore', 'discern_realities', 'parley', 'tech_use', 'aid_or_interfere'],
   weapons_list: ['laspistol', 'stub_revolver', 'lasgun', 'm36_lasgun', 'double_barreled_shotgun', 'pump_action_shotgun', 'box_of_ammo', 'club', 'dagger', 'throwing_dagger', 'short_sword_aka', 'spear', 'long_sword_aka', 'halberd', 'rapier', 'dueling_rapier', 'chainsword'],
   armor_list: ['leather_armor_aka', 'flak_armor', 'carapace_armor', 'shield'],
