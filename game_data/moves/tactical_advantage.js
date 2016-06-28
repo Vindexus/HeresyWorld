ff845de5ec42251cdd759dd8db04e7d43b0442d2
unknown <vindexus@gmail.com>
2016-06-27 22:42:23 -0700

    Added new version of warrior.

---
+++ game_data/moves/tactical_advantage.js
@@ -0,0 +1,7 @@
+module.exports = {
+  description: '<p class="no-indent">Each battle is different and requires a different approach. At the start of each battle, decide which tactic you are actively employing.</p>' +
+  '<ul><li><strong>Defensive</strong>: As long as you don\'t do anything too rash, you gain +1 to all Defy Danger rolls</li>' +
+  '<li><strong>Reactive</strong>: After you are attacked, you can immediately respond and take +1 forward</li>' +
+  '<li><strong>Reckless</strong>: You deal +1d4 damage and take +1d4 damage</li>' +
+  '<li><strong>Precise</strong>: While you remain focused on a single target, you gain +2 piercing against them</li></ul>'
+}
---
+++ game_data/moves/tome_of_war.js
@@ -0,0 +1,4 @@
+module.exports = {
+  name: 'Tome of War',
+  description: '<p class="no-indent">You know war and fighting. Gain +1 on any {{moves.spout_lore.name}} rolls that deal with past battles, renowned heroes, weapons, or armor.</p>'
+}
